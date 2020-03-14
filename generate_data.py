4431ee2c9cc83d8c92475783fbf47ab4babd9a9a
KaviMD <kaviasher@gmail.com>
2020-03-14 11:49:34 -0700

    20 epochs

--- generate_data.py
+++ generate_data.py
@@ -1,15 +1,20 @@
-step = 2
+step = 1
 
 if step == 1:
     from pytube import YouTube
     # List of Videos from: 
     # * https://docs.google.com/document/d/1dTGhGsim4t1xoeWh1I58DQ6xIg_3JI0-dUdBN9170FM/edit
     # * https://www.youtube.com/playlist?list=PLocx3vY5mUKNP-v3Wm1v01kLMvQ_iWMLe
+
+    # Playlists exported with: https://github.com/pierlauro/playlist2links
     with open('videos.txt', 'r') as f:
         videos = f.readlines()
         for video in videos:
-            YouTube(video).streams.first().download("data/raw_videos/", skip_existing=True)
-            print("Downloaded", video)
+            try:
+                YouTube(video).streams.first().download("data/raw_videos/", skip_existing=True)
+                print("Downloaded", video)
+            except:
+                print("Couldn't download", video)
     step = 2
 
 if step == 2:
@@ -27,7 +32,7 @@ if step == 2:
             ret, frame = cap.read()
             if ret == False:
                 break
-            filename = "data/img/"+name+"."+str(i)+".png"
+            filename = "data/img/train/"+name+"."+str(i)+".png"
             if not os.path.isfile(filename):
                 if i % 20 == 0:
                     cv2.imwrite(filename,frame)
--- train.py
+++ train.py
@@ -52,7 +52,7 @@ ngf = 64
 ndf = 64
 
 # Number of training epochs
-num_epochs = 5
+num_epochs = 20 #5
 
 # Learning rate for optimizers
 lr = 0.0002
@@ -307,8 +307,6 @@ plt.ylabel("Loss")
 plt.legend()
 plt.show()
 
-
-# %%
 #%%capture
 fig = plt.figure(figsize=(8,8))
 plt.axis("off")
@@ -317,4 +315,22 @@ ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit
 
 HTML(ani.to_jshtml())
 
+# %%
+# Grab a batch of real images from the dataloader
+real_batch = next(iter(dataloader))
 
+# Plot the real images
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.axis("off")
+plt.title("Real Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+# Plot the fake images from the last epoch
+plt.subplot(1,2,2)
+plt.axis("off")
+plt.title("Fake Images")
+plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+plt.show()
+
+# %%
